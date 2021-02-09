80c4c34fcffac563a88c2da5faa89bf4a109fc63
Bostjan <bostjanzup@gmail.com>
2021-02-09 19:43:03 +0100

    initial firebase

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-let myLibrary = [
-    new Book("The Hobbit", "J. R. R. Tolkien", 1937),
-    new Book("Hitchhiker's guide to the galaxy", "Douglas Adams", 1978),
-    new Book("Atomic Habits", "James Clear", 2018)
-];
+let myLibrary = [];
 
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-    return this;
+/* const dbRefObject = firebase.database().ref().myLibrary;
+dbRefObject.on("value", snap => console.log(snap.val())) */
+
+let database = firebase.database();
+let ref = database.ref("books");
+ref.on("value", gotData, errData);
+
+function gotData(data) {
+    books = data.val();
+    console.log(books);
+    let keys = Object.keys(data.val());
+    console.log(keys);
+    for(let i = 0; i < keys.length; i++)
+    {
+        new Book(books[keys[i]].title, books[keys[i]].author, books[keys[i]].year).displayBook();
+        
+        console.log(books[keys[i]]);
+    }
+    
 }
 
-Book.prototype.displayBook = function() {
-    let booksData = document.querySelector("table");
+function errData(err) {
+    console.log("error");
+    console.log(err.val());
+}
+
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.read = false;
+        return this;
+    }
+
+    deleteBook() {
+        this.library.splice(this.library.indexOf(this), 1);
+    }
+
+    changeBookReadState() {
+        this.read = !this.read;
+    }
+
+    displayBook() {
+        let booksData = document.querySelector("table");
         let tableRow = document.createElement("tr");
-        
+        tableRow.classList.add("singleBook");
+
         let titleCell = document.createElement("td");
         titleCell.textContent = this.title;
         tableRow.appendChild(titleCell);
@@ -27,40 +60,80 @@ Book.prototype.displayBook = function() {
         yearCell.textContent = this.year;
         tableRow.appendChild(yearCell);
 
+        let bookReadCell = document.createElement("td");
+        let bookReadCellPara = document.createElement("p");
+        bookReadCellPara.textContent = this.read.toString();
+
+        bookReadCell.appendChild(bookReadCellPara);
+
+        let readButton = document.createElement("button");
+        readButton.setAttribute("data-id", this.library.indexOf(this))
+        readButton.addEventListener("click", setBookReadButton);
+        bookReadCell.appendChild(readButton);
+
+        tableRow.appendChild(bookReadCell);
+
         let deleteButtonCell = document.createElement("td");
         let deleteButton = document.createElement("button");
+        deleteButton.setAttribute("data-id", this.library.indexOf(this));
         deleteButtonCell.appendChild(deleteButton);
         deleteButton.textContent = "Delete book";
-        deleteButton.addEventListener("click", deleteBook)
+        deleteButton.addEventListener("click", deleteBookEvent);
         tableRow.appendChild(deleteButtonCell);
 
         booksData.appendChild(tableRow);
         return this;
+    }
+
+    addBookToLibrary() {
+        this.library.push(this);
+        ref.push(this);
+        return this;
+    }
+
+    static displayAllBooks() {
+        Book.prototype.library.forEach(book => book.displayBook());
+    }
+
+    static createNewBookForm() {
+        let _title = document.querySelector("#bookTitleInput");
+        let _author = document.querySelector("#bookAuthorInput");
+        let _year = document.querySelector("#bookYearInput");
+        new Book(_title.value, _author.value, _year.value).addBookToLibrary().displayBook();
+        _title.value = "";
+        _author.value = "";
+        _year.value = "";
+
+        
+    }
+
+}
+//sets the library the class works with
+Book.prototype.library = myLibrary;
+
+function deleteBookEvent(elem) {
+    myLibrary.splice(elem.target.getAttribute("data-id"), 1);
+    let allBookRows = document.querySelectorAll(".singleBook");
+    console.log(allBookRows);
+    for(let i = 0; i < allBookRows.length; i++) {
+        allBookRows[i].parentNode.removeChild(allBookRows[i]);
+    }
+    Book.displayAllBooks();
 }
 
-Book.prototype.addBookToLibrary = function () {
-    myLibrary.push(this);
-}
-
-function createNewBook() {
-    let _title = document.querySelector("#bookTitleInput").value;
-    let _author = document.querySelector("#bookAuthorInput").value;
-    let _year = document.querySelector("#bookYearInput").value;
-    new Book(_title, _author, _year).displayBook().addBookToLibrary();
-}
-
-function displayLibrary(library) {
-    library.forEach(book => book.displayBook()); 
-}
-
-function deleteBook(book) {
-
+function setBookReadButton(elem) {
+    let index = elem.target.getAttribute("data-id")
+    myLibrary[index].changeBookReadState();
+    elem.target.parentNode.firstChild.textContent = myLibrary[index].read;
 }
 
 
-displayLibrary(myLibrary);
+/* new Book("The Hobbit", "J. R. R. Tolkien", 1937).addBookToLibrary().displayBook();
+new Book("Hitchhiker's guide to the galaxy", "Douglas Adams", 1978).addBookToLibrary().displayBook();
+new Book("Atomic Habits", "James Clear", 2018).addBookToLibrary().displayBook();
+ */
 
-let neki = new Book(" 213", "21312", 111).displayBook().addBookToLibrary();
+
 
 const addBookButton = document.querySelector("#submitNewBook");
-addBookButton.addEventListener("click", createNewBook);
+addBookButton.addEventListener("click", Book.createNewBookForm);
